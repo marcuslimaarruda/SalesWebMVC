32ad1bd01acab6779992f9ca414b3249fedf0222
Marcus Lima Arrua <45215381+marcuslimaarruda@users.noreply.github.com>
2021-01-20 14:49:54 -0300

    Conectando e criando por code-first a base mysql

--- SalesWebMVC/Models/Departamento.cs
+++ SalesWebMVC/Models/Departamento.cs
@@ -1,9 +1,13 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace SalesWebMVC.Models
 {
     public class Departamento
     {
-        public int id { get; set; }
-        public string Nome { get; set; }
+        public int Id { get; set; }
+        public string None { get; set; }
     }
 }
